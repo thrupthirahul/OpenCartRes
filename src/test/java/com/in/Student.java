5f4063d5ce6b7dfbb4f98eb0e484834f07fe87e5
thrupthirahul <thrupthirahul8@gmail.com>
2025-05-15 11:23:07 +0530

    Message4

---
+++ src/test/java/com/in/Student.java
@@ -0,0 +1,26 @@
+package com.in;
+
+public class Student {
+	
+	String name;
+	int id;
+	String course;
+	
+	public Student(){
+		
+	}
+	
+	public Student(String name, int id,String course) {
+		this.name=name;
+		this.id=id;
+		this.course=course;
+  		System.out.println("Student Constructor");
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "["+this.name+","+this.id+","+this.course+"]";
+	}
+
+}
